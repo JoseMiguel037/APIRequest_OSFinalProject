71e8de521f20c92536e1b19c07e76f018b35b042
JoseMiguel037 <tamayo.jose@correounivalle.edu.co>
2022-03-25 01:11:14 +0000

    first commit

--- APIRequest.sh
+++
@@ -1,5 +0,0 @@
-
-#!/usr/bin/env bash
-# Este script en realidad es para crear un canal en ThingSpeak
-
-curl --header "Content-Type: application/json" --request POST --data '{"api_key":"Q489PVV7XBG2VYWZ","name":"APIRequest_Project","field1":"Name","field2":"Category","field3":"Group","field4":"Html Code"}' https://api.thingspeak.com/channels.json
